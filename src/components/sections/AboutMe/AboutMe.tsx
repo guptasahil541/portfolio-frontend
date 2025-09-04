e97340d8823f4908aa8a2a4dd8cab1e21594662e
Sahil Gupta <guptasahil541@gmail.com>
2025-09-05 00:52:13 +0530

    Refactored heading elements to common ui components

--- src/components/sections/AboutMe/AboutMe.tsx
+++ src/components/sections/AboutMe/AboutMe.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import { SectionHeading, SectionSubHeading } from '@/components/ui';
+
 import styles from './AboutMe.module.css';
 
 export const AboutMe = () => {
@@ -10,21 +12,21 @@ export const AboutMe = () => {
             className={classNames('container', styles.about_me_container)}
         >
             <div className={styles.section_header}>
-                <h1 className={styles.section_heading}>
+                <SectionHeading>
                     Developer. Designer. <br />A Storyteller in UI
-                </h1>
-                <h3 className={styles.section_sub_heading}>ABOUT ME</h3>
+                </SectionHeading>
+                <SectionSubHeading>ABOUT ME</SectionSubHeading>
             </div>
-            <div className={styles.section_content_container_outer}>
-                <div className={styles.section_content_spacer}></div>
-                <div className={styles.section_content_container_inner}>
-                    <p className={styles.section_content}>
+            <div className={styles.text_container_outer}>
+                <div aria-hidden></div>
+                <div className={styles.text_container_inner}>
+                    <p>
                         I’m a Full Stack Developer with 3.5 years of experience
                         working across frontend and backend technologies. Over
                         the years, I’ve built everything from sleek, interactive
                         UIs to robust, scalable applications.
                     </p>
-                    <p className={styles.section_content}>
+                    <p>
                         I enjoy bridging creativity and engineering to create
                         digital products that are both visually engaging and
                         technically sound.
